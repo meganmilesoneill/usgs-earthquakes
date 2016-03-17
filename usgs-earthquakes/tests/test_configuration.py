e9ab45ee9f0cc9eff8bb10b30f990701f603f12d
meganmilesoneill <meganmilesoneill@gmail.com>
2016-03-16 23:47:10 -0700

    created root usgs-earthquakes package folder

--- usgs-earthquakes/tests/test_configuration.py
+++ usgs-earthquakes/tests/test_configuration.py
@@ -5,7 +5,7 @@ class TestConfiguration(unittest.TestCase):
 
 	def test_loadTestConfig(self):
 		testConfig = configuration.Configuration("test")
-		self.assertEqual(testConfig.database["name"], "usgs-earthquakes-test")
+		self.assertEqual(testConfig.database["name"], "usgs_earthquakes_test")
 
 if __name__ == "__main__":
 	unittest.main()
